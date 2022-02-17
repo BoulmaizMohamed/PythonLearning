a733c2a85eb3e8309c9f579cb38cd16773433329
BoulmaizMohamed <90794998+BoulmaizMohamed@users.noreply.github.com>
2022-02-17 10:49:30 +0100

    Create PythonString.py

---
+++ PythonString.py
@@ -0,0 +1,10 @@
+#in Python using (" or ') using for a string doesn't affect how it behaves in any way.
+
+print('Hello World')
+print("Hello World")
+
+
+
+#you must use backslash \ before double quate
+
+print("Hello\'s World")    #output: Hello's World
