1fa24eaf785f51db838f5e8f0fa5d1c0cd8e6b4c
BoulmaizMohamed <90794998+BoulmaizMohamed@users.noreply.github.com>
2022-02-17 10:36:04 +0100

    Create PythonOperations.py

---
+++ PythonOperations.py
@@ -0,0 +1,26 @@
+#Phython support calculation in print method
+
+#Phython store the operations below as integer
+print(2+5)
+print(5+2-1)
+print(2*2(3+4))
+
+#Phython store the operations below as float 
+print(0.90)
+print(3/4)
+print(9*10.0)
+print(4+5.1)
+
+#Phython can use exponentiation which raising of number to the power of another
+print(2**2)
+
+#Phython can use floor division to determine quotient of a devision 
+print(10//4)         #output is 2
+
+#Phython can use modulo operator to determine remainder of a devision 
+print(9%4)         #output is 1
+
+
+
+
+
