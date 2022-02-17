52972a104fbe1dfe19d62056829d578ab514d855
BoulmaizMohamed <90794998+BoulmaizMohamed@users.noreply.github.com>
2022-02-17 11:03:00 +0100

    Create 5-StringOperations.py

---
+++ 5-StringOperations.py
@@ -0,0 +1,12 @@
+print("cheez" + 'burger')                                        #output: cheezburger
+
+print("5" + "6")                                                 #output: 56
+
+
+#in python you can use multiplication usin g interger
+
+print("cheez" * 4)                                                #output: cheezcheezcheezcheez
+
+print(4 * "cheez")                                                #output: cheezcheezcheezcheez
+
+print(4 * "9")                                                    #output: 9999
